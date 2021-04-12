2f1146d6637aaec7a16d8612f10665b0bcf2a88f
sivan2203 <sivan9458@gmail.com>
2021-04-12 15:42:47 +0700

    upload main.c

--- src/main.c
+++ src/main.c
@@ -4,26 +4,5 @@
 #include "myTerm.h"
 
 int main() {
-
-  /*bc_printbigchar(zero, 10, 0, 0, 7);
-  bc_printbigchar(one, 10, 9, 0, 7);
-  bc_printbigchar(two, 10, 17, 0, 7);
-  bc_printbigchar(three, 10, 25, 0, 7);
-  bc_printbigchar(four, 10, 33, 0, 7);
-  bc_printbigchar(five, 10, 41, 0, 7);
-  bc_printbigchar(six, 10, 49, 0, 7);
-  bc_printbigchar(seven, 10, 57, 0, 7);
-  bc_printbigchar(eight, 10, 65, 0, 7);
-  bc_printbigchar(nine, 10, 73, 0, 7);
-  bc_printbigchar(plus, 10, 80, 0, 7);
-  bc_printbigchar(F, 10, 87, 0, 7);
-
-
-  int* value = malloc(sizeof(int));
-  bc_getbigcharpos(zero, 7, 4, value);
-  printf("\n\n\n\nvalue = %d\n", *value);*/
-
   interface();
-
-  //mt_clrscr();
 }
